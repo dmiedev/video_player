de98d29dccfe89aa7e449f4b2194be122c562f04
Dmitry Egorov <egorovdima60@gmail.com>
2022-10-02 15:30:59 +0200

    feat: implement home page

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -14,11 +14,64 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class _HomeView extends StatelessWidget {
+class _HomeView extends StatefulWidget {
   const _HomeView();
 
   @override
+  State<_HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<_HomeView> {
+  final _videoLinkFieldKey = GlobalKey<FormFieldState<String>>();
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text(
+                'Video Player',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              const SizedBox(height: 50),
+              SizedBox(
+                width: 350,
+                child: TextFormField(
+                  key: _videoLinkFieldKey,
+                  decoration:
+                      const InputDecoration(hintText: 'Enter video URL...'),
+                  validator: _validateVideoUrl,
+                ),
+              ),
+              const SizedBox(height: 30),
+              FloatingActionButton.extended(
+                label: const Text('WATCH'),
+                onPressed: _handleWatchButtonPress,
+              ),
+              const SizedBox(height: 50),
+              const Text('Note: Video Player only supports HTTPS links.'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  String? _validateVideoUrl(String? url) {
+    if (url == null) {
+      return null;
+    } else if (!url.startsWith('https://')) {
+      return 'This is not a HTTPS link!';
+    }
+    return null;
+  }
+
+  void _handleWatchButtonPress() {
+    if (!_videoLinkFieldKey.currentState!.validate()) {
+      return;
+    }
+    // context.read<HomeBloc>().add();
   }
 }
